ec4290802461aa8f74cc7dc578f725dff5110161
Ttante <tyler.dinner@gmail.com>
2014-04-04 10:12:57 -0400

    content work

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,6 @@
-var ready = (function () {
-  var isReady = false;
-  return function ready (fn) {
-    if (true === isReady || 'complete' === document.readyState) {
-      isReady = true;
-      setTimeout(fn);
-      return;
-    } else {
-      document.addEventListener('DOMContentLoaded', function () {
-        console.log('ready')
-        isReady = true;
-        fn();
-      });
-    }
-  };
-}());
+window.addEventListener('load', fucntion()) {
 
-ready(function () {
+// document.getElementById('#header').css('width', '100%');
 
 
-});
+}, false);
